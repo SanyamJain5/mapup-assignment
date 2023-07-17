ac92962fd3be47881af0b521dfcb2e97c2815bae
SanyamJain <sanyamjain5548@gmail.com>
2023-07-17 23:21:53 +0530

    mapup assignment

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const authRoutes = require('./auth');
+const mapRoutes = require('./map');
+
+router.use('/auth', authRoutes);
+router.use('/map', mapRoutes);
+
+// starting route
+router.get('/', (req, res) => {
+    res.send('Welcome to the API'); 
+}); 
+
+module.exports = router; 
